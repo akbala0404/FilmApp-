79e77abeddc5e62915c7a73cbdd2fbcf25ddfba8
akbala0404 <81943684+akbala0404@users.noreply.github.com>
2022-04-01 00:00:28 +0600

    Initial Commit

---
+++ FilmApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FilmApp
+//
+//  Created by Акбала Тлеугалиева on 4/1/22.
+//  Copyright © 2022 Akbala Tleugaliyeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
